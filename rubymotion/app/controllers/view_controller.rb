5ecdb7e02520baeaa05d55de2b223054a797954b
Devon Blandin <dblandin@gmail.com>
2014-03-14 17:50:52 -0500

    Bring ScrollCell out of ViewController

--- rubymotion/app/controllers/view_controller.rb
+++ rubymotion/app/controllers/view_controller.rb
@@ -17,29 +17,4 @@ class ViewController < UIViewController
   def tableView(table_view, cellForRowAtIndexPath: index_path)
     ScrollCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier: 'IDENTIFIER')
   end
-
-  class ScrollCell < UITableViewCell
-    def initWithStyle(style, reuseIdentifier: reuse_identifier)
-      super.tap do |cell|
-        cell.selectionStyle = UITableViewCellSelectionStyleNone
-
-        cell.setup
-      end
-    end
-
-    def setup
-      @scroll_view = UIScrollView.alloc.init
-      @scroll_view.backgroundColor = UIColor.redColor
-      @scroll_view.delegate        = self
-      @scroll_view.frame           = contentView.frame
-
-      contentView.addSubview(@scroll_view)
-
-      @scroll_view.contentSize = [contentView.frame.size.width * 2, contentView.frame.size.height]
-    end
-
-    def scrollViewWillEndDragging(scroll_view, withVelocity: velocity, targetContentOffset: target_content_offset)
-      p 'scrollViewWillEndDragging'
-    end
-  end
 end
